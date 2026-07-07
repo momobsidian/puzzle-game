e3c8afe4b040951cc82637593ed1b1a7bdefe534
momobsidian <momobsidian@gmail.com>
2026-07-07 20:01:10 +0600

    Update UI to 3D old-game theme and fix server session bug

--- server.js
+++ server.js
@@ -340,7 +340,7 @@ const server = http.createServer((req, res) => {
         broadcastToAdmins({
           type: 'gameUpdate',
           data: {
-            sessionId,
+            sessionId: ip,
             ...stats,
             lastUpdate: Date.now()
           }
